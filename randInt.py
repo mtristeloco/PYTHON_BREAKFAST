a5d81c98545dd7d27a37f7f5d21061eb4c9aff0b
Michal Kalinic <michal.kalinic@gmail.com>
2012-03-20 17:41:16 +0100

    erste Versuch zum Uploaden

--- randInt.py
+++ randInt.py
@@ -10,19 +10,23 @@ __author__ = 'Michal'
 # parameter von user einlesen, und check
 n = int(sys.argv[1])
 m = int(sys.argv[2])
-
+outputfile = (input('Geben Sie die Name der Datei ein:'))
 
 '''
 n = int(input('Geben Sie anzahl:')) # anzahl der zahlen
 m = int(input('Bereich:')) # bereich
 '''
 
+fobj = open (outputfile, "w")
 
 # loops schleife
 for i in range(0,n):
     # zufalszahl generieren
     my_number = random.randint(0, m)
     #zufalszahl ausgenen
+'''
     print (my_number)
+'''
+    fobj.write('my_number')
 
-
+fobj.close()
