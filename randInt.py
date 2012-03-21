92d040b95ee34703de2d5f355c80558409a7a389
Michal Kalinic <michal.kalinic@gmail.com>
2012-03-21 15:29:18 +0100

    Random save to file

--- randInt.py
+++ randInt.py
@@ -1,32 +1,30 @@
 import random
-import sys
 
-__author__ = 'Michal'
+__author__ = 'MK'
 
 
 # pseudocode
 # random integers
 
 # parameter von user einlesen, und check
-n = int(sys.argv[1])
-m = int(sys.argv[2])
-outputfile = (input('Geben Sie die Name der Datei ein:'))
+# n = int(sys.argv[1])
+# m = int(sys.argv[2])
 
-'''
 n = int(input('Geben Sie anzahl:')) # anzahl der zahlen
 m = int(input('Bereich:')) # bereich
-'''
 
-fobj = open (outputfile, "w")
+outputfile = (input('Geben Sie die Name der Datei ein:')) #Name der Datei
+
+datei = open (outputfile, "a") # "r", "w", "a"
 
 # loops schleife
 for i in range(0,n):
     # zufalszahl generieren
     my_number = random.randint(0, m)
-    #zufalszahl ausgenen
-'''
-    print (my_number)
-'''
-    fobj.write('my_number')
+    #zufalszahl ausgebeen
+    #print (my_number)
 
-fobj.close()
+    #schreibe Ergebnis in die Datei
+    datei.write(str(my_number) + '\n')
+
+datei.close()
