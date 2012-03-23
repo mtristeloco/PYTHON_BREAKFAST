9d1b217d8995685d1ec09d1863180e5eeb9bb42a
Michal Kalinic <michal.kalinic@gmail.com>
2012-03-23 16:06:45 +0100

    changes added

---
+++ mirror_word.py
@@ -0,0 +1,20 @@
+##############################################################
+# Program revers String
+# Python 3.2
+##############################################################
+import sys
+
+__author__ = 'MK'
+
+# global
+user_word = ''
+
+# define revers function rev
+def rev(user_word): return user_word[::-1]
+
+# entry point
+user_word = (sys.argv[1])
+user_word = rev(user_word)
+print(user_word)
+
+
