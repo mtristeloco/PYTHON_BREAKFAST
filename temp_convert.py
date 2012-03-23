8e23ec6db727916b2e019e8748e0cf10b059b18a
Michal Kalinic <michal.kalinic@gmail.com>
2012-03-23 16:43:15 +0100

    changes added

---
+++ temp_convert.py
@@ -0,0 +1,24 @@
+import sys
+
+__author__ = 'MK'
+
+# global parameters
+temp_val = 0
+degree = ['Celsius', 'Farenheit']
+
+
+#main program
+temp_val = (sys.argv[1])
+degree = (sys.argv[2])
+
+
+if degree =='Celsius':
+    temp_val=float(temp_val)
+    far_value = temp_val * 9.0 / 5.0 + 32
+    print("%.2f" %far_value)
+
+else:
+    temp_val=float(temp_val)
+    celsius_value = (temp_val - 32 ) * 5 / 9
+    print("%.2f" %celsius_value)
+
