78496fdcc87fc9a3f52ca41502e572539870ea3d
Cornel MICU <c.micu@me.com>
2012-04-18 20:22:21 +0200

    pascals triangle

--- pascal_triangle_symbolic.py
+++ pascal_triangle_symbolic.py
@@ -10,19 +10,40 @@ import sys
 
 __author__ = 'Michal'
 
-character = sys.argv[1]
-n = int(sys.argv[2])
+#character = sys.argv[1]
+character = '*' # sys.argv[1]
+n = 8 # int(sys.argv[2])
 draw = ''
+
 def pascal_triangel():
-    for l in range(1, n):
-        draw_line = draw + ' '
+
+    for l in range(1, n):  # i,j,k   foo, bar
+        draw_line = draw + ' '  # => '________'
+    print('Drawline [' + draw_line + ']')
     for i in range(0, n):
         draw_line = draw[1:]
         draw_line = draw + ' ' + character
-        print (draw)
+        #print (draw_line)
 
 pascal_triangel()
 
+ #********
+def sterne(n):
+    stern = ''
+    for i in range(0,8-n):
+        stern = ' ' + stern
+    for i in range(0,n):
+        stern = stern + '? '
+    return stern
+
+for i in range(0,n):
+    print(sterne(i))
+
+    '''
+for i in range(0,8):
+    stars = ' '*(8-i)+'* '*(i)
+    print(stars)
+    '''
 # Why I cannot print it?
 
 '''
